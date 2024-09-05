7043aba80aa9ff107a302a83f6770bc9b2aa6b34
Joilson Capemba <joilsoncapemba@gmail.com>
2024-09-05 16:10:03 +0100

    chore: configuracao do reacr-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
 
 export function App() {
 
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <h1>Recomeçando</h1>
+        <BrowserRouter>
+          <Router/>
+        </BrowserRouter>
         <GlobalStyle/>  {/* Este componente adiciona o estilo global na aplicacao */}
       </ThemeProvider>
     </>
